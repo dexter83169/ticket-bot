0a6f9780c01409714bf1da1d718224f53bfd7a77
dexter83169 <dreamgeneve@gmail.com>
2026-03-08 13:13:15 +0100

    Atualização do bot

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot online");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
+
 require("dotenv").config();
 
 const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, EmbedBuilder } = require("discord.js");
