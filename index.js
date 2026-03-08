a1fd2b9a17264320531b56dbd1698e931442bb89
dexter83169 <dreamgeneve@gmail.com>
2026-03-08 14:00:25 +0100

    Atualização do bot

--- index.js
+++ index.js
@@ -248,4 +248,20 @@ client.on(Events.InteractionCreate, async interaction => {
 // ===============================
 // LOGIN
 // ===============================
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  console.log("Interaction received:", interaction.commandName);
+});
+
+
+
 client.login(process.env.TOKEN);
