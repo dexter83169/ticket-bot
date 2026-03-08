62dfb46b675c75d3e662aac73544215b93945a4a
dexter83169 <dreamgeneve@gmail.com>
2026-03-08 14:16:14 +0100

    Atualização do bot

--- index.js
+++ index.js
@@ -257,6 +257,29 @@ client.on("interactionCreate", async (interaction) => {
   console.log("Interaction received:", interaction.commandName);
 });
 
+const { REST, Routes } = require("discord.js");
+const commands = [
+  {
+    name: "reply",
+    description: "Test reply command"
+  }
+];
 
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Refreshing slash commands...");
+
+    await rest.put(
+      Routes.applicationGuildCommands("1470122326191247593", "1447731387250507857"),
+      { body: commands }
+    );
+
+    console.log("Commands registered ✅");
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 client.login(process.env.TOKEN);
